a962c1737123b86cbb47c6d7c69a1b0afe2cf336
ASxx <123456789@qq.com>
2019-03-16 16:16:25 +0800

    创建项目

---
+++ src/component/logo/logo.js
@@ -0,0 +1,12 @@
+import React,{ Fragment } from 'react';
+
+class Logo extends React.Component{
+
+  render(){
+    return(
+      <div className='logo-contanier'>
+        <img src={logoImg} alt=''/>
+      </div>
+    )
+  }
+}
---
+++ src/container/login/login.js
@@ -0,0 +1,10 @@
+import React,{ Fragment } from 'react';
+
+class Login extends React.Component{
+
+  render(){
+    return(
+      <h1>这是登录页</h1>
+    )
+  }
+}
